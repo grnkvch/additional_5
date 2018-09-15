1fdb46a9198cbb228fa2dc54266661be34e08064
grnkvch <43145804+grnkvch@users.noreply.github.com>
2018-09-15 18:12:43 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var cor = [0];
+
+    function ini(n) {
+      var op = false;
+      for (var j = 0; j<bracketsConfig.length;j++) {
+        if (str[n] == bracketsConfig[j][0]) {
+          cor[0]++;
+          cor.push(j);
+          op = true;
+          break;
+        }
+      }
+      if (!op) {
+        for (var j = 0; j<bracketsConfig.length;j++) {
+        if (str[n] == bracketsConfig[j][1]) {
+          cor[0]++;
+          cor.push(j);
+          break;
+        }
+      }
+      }
+    } 
+
+    ini(0);
+
+  for (var i = 1; i<str.length;i++) {
+    if (cor.length == 1) {
+      ini(i);
+      i++;
+    }
+    for (var j = 0; j<bracketsConfig.length;j++) {
+      if (str[i] == bracketsConfig[cor[cor.length-1]][1]) {
+        cor[0]--;
+        cor.pop()
+        break;
+      } else {
+        if (str[i] == bracketsConfig[j][0]) {
+          cor[0]++;
+          cor.push(j)
+          break;
+        }
+      }
+    }
+  }
+  if (cor[0] == 0) {
+   return true; 
+  } else {
+   return false;
+  }
 }
